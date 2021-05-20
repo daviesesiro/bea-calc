db628f1ae26bcf1d8f06ed181768cd5623466beb
daviesesiro <ojurereoluwad@gmail.com>
2021-05-20 22:02:32 +0100

    fixed deployment bug

--- pages/component/Transaction.tsx
+++ pages/component/Transaction.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 const Transaction: React.FC<Props> = ({ receiving, amount, transactionDate, title }) => {
-    console.log(receiving)
+    amount = amount || 0;
     const Icon = receiving ? <svg width={24} height={14} viewBox="0 0 24 14" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M23 1L13.5 10.5L8.5 5.5L1 13" stroke="#6DFF0E" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
         <path d="M17 1H23V7" stroke="#6DFF0E" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
