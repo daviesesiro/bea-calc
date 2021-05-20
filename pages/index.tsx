d70368d76a9422a745b6a4d7055c4168db2ccf73
daviesesiro <ojurereoluwad@gmail.com>
2021-05-20 13:15:32 +0100

    removed window10 images

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Metas from './component/Metas'
 
 export default function Home() {
   return (
-    <div className='h-screen bg-green-400'>
+    <div className='h-screen bg-red-400'>
       <Metas />
 asdf
     </div>
