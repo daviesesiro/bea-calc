f48cf56f76ab06c68764a503ecf4c26b34a3313c
daviesesiro <ojurereoluwad@gmail.com>
2021-05-20 13:02:07 +0100

    initial

---
+++ pages/component/Metas.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+const Metas = () => <Head>
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="uc-fitscreen=yes" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+    <meta name="keywords" content="Calculator, Beauty, Transaction, Bank" />
+    <title>Beauty Calculator</title>
+    <meta name="description" content="An app for Beauty to calculate transactions on her bank account" />
+    <link rel="manifest" href="manifest/manifest.json"></link>
+    <link rel="icon" href="/favicon.ico" />
+</Head>
+
+export default Metas;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Metas from './component/Metas'
+
+export default function Home() {
+  return (
+    <div className='h-screen bg-green-400'>
+      <Metas />
+asdf
+    </div>
+  )
+}
