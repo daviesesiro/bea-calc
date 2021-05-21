2d898101a5054b7b70974fbac7b13fc38e095881
daviesesiro <ojurereoluwad@gmail.com>
2021-05-22 00:58:40 +0100

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,11 +8,12 @@ interface Props {
 }
 
 const Header = ({ title, actions, home }: Props) => {
+    const router = useRouter()
     return (
         <React.Fragment>
             <div className='rounded-b-xl fixed top-0 left-0 right-0 flex items-center justify-between px-4 py-3 bg-white'>
                 {!home &&
-                    <button className='focus:outline-none p-2 mr-4' onClick={useRouter().back}>
+                    <button className='focus:outline-none p-2 mr-4' onClick={() => router.push('/')}>
                         <svg width={16} height={16} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M15 8H1" stroke="black" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
                             <path d="M8 15L1 8L8 1" stroke="black" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
--- pages/person/[id].tsx
+++ pages/person/[id].tsx
@@ -1,23 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Header from '../../components/Header'
 import Layout from '../../components/Layout'
 import Transaction from '../../components/Transaction'
 import Modal from '../../components/Modal'
+import DB from '../../utils/DB'
 
 const Index = () => {
     const router = useRouter()
-    const { id } = router.query
-
-    console.log(router)
+    let { id } = router.query
+    if (typeof window != 'undefined') {
+        id = id ||
+            window.location.pathname.split('person/')[1]
+    }
     const params = new URLSearchParams(router.asPath.split('?')[1])
     const personName = params.get('name')
+    const initialBalance = params.get('balance')
 
-    if (personName == '') router.back()
 
     const [showAddTran, setShowAddTran] = useState(false)
-    const balance = 3453300
-    const transactionDate = new Date()
+    const [balance, setBalance] = useState<number>(Number(initialBalance))
+    const date = new Date()
+    useEffect(() => {
+        const init = async () => {
+            const balance = (await DB.getPersonById(id as string)).balance;
+            setBalance(balance)
+        }
+
+        init()
+    }, [balance, setBalance])
+
+
+    const [formState, setformState] = useState({ title: '', amount: 0, description: '', type: 'Receiving' })
+
+    const handleOnChange = (e) => setformState(prev => ({ ...prev, [e.target.name]: e.target.name == 'amount' ? Number(e.target.value) : e.target.value }))
+
+    const handleSubmit = () => {
+
+    }
     return (
         <Layout >
             <Header title={personName} actions={[{
@@ -37,41 +57,51 @@ const Index = () => {
 
             {/* Transaction */}
             <div className='mt-8'>
-                <Transaction amount={4400} title='hello 2' receiving description="hello" transactionDate={transactionDate} />
-                <Transaction amount={1000} title='hello' receiving={false} description="hello" transactionDate={transactionDate} />
-                <Transaction amount={5500} title='hello 2' receiving description="hello" transactionDate={transactionDate} />
-                <Transaction amount={66000} title='hello 2' receiving description="hello" transactionDate={transactionDate} />
-                <Transaction amount={36000} title='hello' receiving={false} description="hello" transactionDate={transactionDate} />
+                <Transaction amount={4400} title='hello 2' receiving description="hello" transactionDate={date} />
+                <Transaction amount={1000} title='hello' receiving={false} description="hello" transactionDate={date} />
             </div>
 
 
             {showAddTran &&
-                <Modal closeModal={() => setShowAddTran(false)} subtitle={`For ${id}`} title='Add a transaction' >
+                <Modal closeModal={() => setShowAddTran(false)} subtitle={`For ${personName}`} title='Add a transaction' >
                     <div>
                         <div className='mb-4'>
                             <label className='block mb-1' htmlFor="title">*Title</label>
-                            <input
+                            <input onChange={handleOnChange}
+                                value={formState.title}
                                 className='focus:outline-none w-full p-3 text-lg bg-gray-100 rounded-lg'
                                 placeholder="For cement" id='title' type="text" name='title' />
                         </div>
                         <div className='mb-4'>
                             <label className='block mb-1' htmlFor="amount">*Amount</label>
-                            <input
+                            <input onChange={handleOnChange}
+                                value={formState.amount}
                                 className='focus:outline-none w-full p-3 text-lg bg-gray-100 rounded-lg'
-                                placeholder="1,000" id='amount' type="text" name='amount' />
+                                placeholder="1,000" id='amount' type="number" name='amount' />
                         </div>
-                        <div className='mb-10'>
+                        <div className='mb-4'>
                             <label className='block mb-1' htmlFor="description">*Descripton</label>
-                            <textarea
+                            <textarea onChange={handleOnChange}
+                                value={formState.description}
                                 className='focus:outline-none w-full p-3 text-lg bg-gray-100 rounded-lg'
                                 placeholder="Write something to describe the transaction" id='description' name='description' />
                         </div>
+                        <div className='mb-10 text-center align-middle'>
+                            <input checked={formState.type == 'Receiving'}
+                                onChange={handleOnChange} className='w-4 h-4 mr-2' type="radio"
+                                name="type" id="type" value="Receiving" />Receiving
+                            <input checked={formState.type == 'Sending'}
+                                onChange={handleOnChange} className='w-4 h-4 ml-4 mr-2' type="radio"
+                                name="type" id="type" value='Sending' />Sending
+                        </div>
 
-                        <button className='active:bg-blue-800 focus:outline-none block w-full py-3 text-sm font-normal text-white bg-blue-700 rounded-lg'>Add Transaction</button>
+                        <button onClick={handleSubmit} className='active:bg-blue-800 focus:outline-none block w-full py-3 text-sm font-normal text-white bg-blue-700 rounded-lg'>Add Transaction</button>
                     </div>
                 </Modal>}
         </Layout>
     )
 }
+
+
 
 export default Index
